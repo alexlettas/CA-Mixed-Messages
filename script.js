d099b0a9e96ab1b5908df372112f827a7e55f27e
alexlettas <alex.lettas@gmail.com>
2021-07-27 19:44:56 +1000

    yep

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // create 3 variables with related theme: D&D Character Generators
 // Assign to an object
-const yourName = ['Razor Wardell', 'Thalytiera', 'Tharran', 'Renia', 'Saldrel, Eamarter', 'Alelas', 'Tarakrit', 'Reyna']
+const yourName = ['Razor Wardell', 'Thalytiera', 'Tharran', 'Renia', 'Saldrel, Eamarter']
 const race = ['Nightborne', 'Orc', 'Blood Elf', 'Human', 'Draenei', 'Undead', 'Dwarf']
-const spec  = ['Mage', 'Warlock', 'Priest', 'Rogue', 'Death Knight', 'Paladin', 'Demon Hunter', 'Monk']
+const spec  = ['Mage', 'Warlock', 'Priest', 'Rogue', 'Death Knight', 'Paladin']
 
 const character = {
 name: yourName,
@@ -12,7 +12,19 @@ spec: spec
 
 const numberGen = num => {
     return Math.floor(Math.random() * num)
-    
 }
 
+const messageCreation = object => {
+    let finalMessage = {};
+    for(let item in character){
+        let randomIdx = numberGen(item.length);
+        finalMessage[item] = character[item][randomIdx];
+    }
+    return finalMessage
+}
 
+const generateMessage = () => {
+    let finalMessage = messageCreation();
+    console.log(`You are ${finalMessage.name}, the ${finalMessage.race} ${finalMessage.spec}`)
+
+}
