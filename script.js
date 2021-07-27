423fa1d36cfb96393e0d6b8a4f18fc55e5d64d8f
alexlettas <alex.lettas@gmail.com>
2021-07-27 17:44:49 +1000

    compiled quotes into single array

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 // create variables with random quotes
 // assign them to a function that logs random quotes?
 
-const inspirationalQuote = ['Life is what you make of it']
+const randomQuotes = ['Life is what you make of it', 
+'What\'s the deal with time? AM I RIGHT!? *pause for laughter*', 
+'You know they tell you an apple a day keeps the doctor away, but my brother Larry is still around']
 
-const funnyQuote = ['What\'s the deal with time? AM I RIGHT!? *pause for laughter*']
 
-const unhelpfulQuote = ['You know they tell you an apple a day keeps the doctor away, but my brother Larry is still around']
+const randomMessage = object => {
+    for(let key in object){
 
-const quoteObject = {
-    
-}
-const randomMessage = arr => {
-
+    }
 }
 
