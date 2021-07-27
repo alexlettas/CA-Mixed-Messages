e1acbd970d0a891751113c9540ad15e9bced042a
alexlettas <alex.lettas@gmail.com>
2021-07-27 17:38:49 +1000

    Generating Quotes

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello There!');
+// create variables with random quotes
+// assign them to a function that logs random quotes?
+
+const inspirationalQuote = ['Life is what you make of it']
+
+const funnyQuote = ['What\'s the deal with time? AM I RIGHT!? *pause for laughter*']
+
+const unhelpfulQuote = ['You know they tell you an apple a day keeps the doctor away, but my brother Larry is still around']
+
+const quoteObject = {
+    
+}
+const randomMessage = arr => {
+
+}
+
