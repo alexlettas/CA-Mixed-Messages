8303f4103bcf30b2b9ac99b14e1ecdba5a601f04
alexlettas <alex.lettas@gmail.com>
2021-07-28 18:59:09 +1000

    adding complexit

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // create 3 variables with related theme: D&D Character Generators
 // Assign to an object
-// const yourName = ['Thalytiera', 'Tharran', 'Renia', 'Saldrel', 'Eamarter']
-// const race = ['Nightborne', 'Orc', 'Blood Elf', 'Human', 'Draenei', 'Undead', 'Dwarf', 'Worgen']
-// const spec  = ['Mage', 'Warlock', 'Priest', 'Rogue', 'Death Knight', 'Paladin', 'Shaman']
+const yourName = ['Thalytiera', 'Tharran', 'Renia', 'Saldrel', 'Eamarter']
+const race = ['Nightborne', 'Orc', 'Blood Elf', 'Human', 'Draenei', 'Undead', 'Dwarf', 'Worgen']
+const spec  = ['Mage', 'Warlock', 'Priest', 'Rogue', 'Death Knight', 'Paladin', 'Shaman']
 //const specialisation = ['Arcane', 'Frost', 'Fire', 'Blood', 'Unholy', 'Retribution', 'Assasination']
 
 const character = {
-    name: ['Thalytiera', 'Tharran', 'Renia', 'Saldrel', 'Eamarter','Tarakrit', 'Rikaul'],
-    race: ['Nightborne', 'Orc', 'Blood Elf', 'Human', 'Draenei', 'Undead', 'Dwarf', 'Worgen'],
-    spec: ['Mage', 'Warlock', 'Priest', 'Rogue', 'Death Knight', 'Paladin', 'Shaman'],
+    name: yourName,
+    race: race,
+    spec: spec,
 }
 
 const numberGen = num => {
@@ -16,9 +16,9 @@ const numberGen = num => {
 }
 
 
-const nameGeneration = character.name[Math.floor(Math.random() * character.name.length)];
-const raceGeneration = character.race[Math.floor(Math.random() * character.race.length)];
-const specGeneration = character.spec[Math.floor(Math.random() * character.spec.length)];
+const nameGeneration = character.name[numberGen(character.name.length)];
+const raceGeneration = character.race[numberGen(character.race.length)];
+const specGeneration = character.spec[numberGen(character.spec.length)];
 
 const output = `You are ${nameGeneration}, the ${raceGeneration} ${specGeneration} `
 
