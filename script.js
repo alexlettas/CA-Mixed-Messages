f6602c05ef9442c5f69b3f0b3160e411d18b266a
alexlettas <alex.lettas@gmail.com>
2021-07-28 19:44:54 +1000

    Everything working as intended :)

--- script.js
+++ script.js
@@ -50,10 +50,35 @@ const raceGeneration = character.race[numberGen(character.race.length)];
 const specGeneration = character.spec[numberGen(character.spec.length)];
 //assigning a variable for each core character component
 
-const output = `You are ${nameGeneration}, the ${raceGeneration} ${specGeneration} `
+const findSpec = []
+
+    if (specGeneration === 'Mage'){
+        findSpec.push(character.specialisation.mage[numberGen(character.specialisation.mage.length)]);
+    } else if (specGeneration === 'Warlock'){
+        findSpec.push(character.specialisation.warlock[numberGen(character.specialisation.warlock.length)]);
+    } else if (specGeneration === 'Priest'){
+        findSpec.push(character.specialisation.priest[numberGen(character.specialisation.priest.length)]);
+    } else if (specGeneration === 'Rogue'){
+        findSpec.push(character.specialisation.rogue[numberGen(character.specialisation.rogue.length)])
+    } else if (specGeneration === 'Death Knight'){
+        findSpec.push(character.specialisation.deathKnight[numberGen(character.specialisation.deathKnight.length)])
+    } else if (specGeneration === 'Paladin'){
+        findSpec.push(character.specialisation.paladin[numberGen(character.specialisation.paladin.length)])
+    } else if (specGeneration === 'Shaman'){
+        findSpec.push(character.specialisation.shaman[numberGen(character.specialisation.shaman.length)])
+    } else if (specGeneration === 'Monk'){
+        findSpec.push(character.specialisation.monk[numberGen(character.specialisation.monk.length)])
+    } else if (specGeneration === 'Hunter'){
+        findSpec.push(character.specialisation.hunter[numberGen(character.specialisation.hunter.length)])
+    } else if (specGeneration === 'Demon Hunter'){
+        findSpec.push(character.specialisation.demonHunter[numberGen(character.specialisation.demonHunter.length)])
+    } else if (specGeneration === 'Druid'){
+        findSpec.push(character.specialisation.druid[numberGen(character.specialisation.druid.length)])
+    } 
+//randomly pick a spec for each class that's picked
+
+const output = `You are ${nameGeneration}, the ${raceGeneration} ${specGeneration}. Your specialisation is ${findSpec.join('')}. Good luck ${nameGeneration} and happy hunting!`
 //something to print to the console :) 
 
 console.log(output)
 //done!
-
-
